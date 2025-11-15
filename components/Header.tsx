47c78caacd3165b50825ab57d4bf795289a746c7
greicioliveira01-web <greicioliveira01@gmail.com>
2025-11-15 12:15:16 -0300

    Add Header component with title 'Coreando Funny'

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header style={{padding: "16px 0"}}>
+      <h1 style={{margin:0}}>Coreando Funny</h1>
+    </header>
+  );
+}
